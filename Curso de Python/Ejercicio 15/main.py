15ef0920217da170e3e6885d9423bc9e84f70821
PabloChavez03 <pablooscarchavez@gmail.com>
2022-08-27 00:01:16 -0300

    ejercicio 15 terminado

---
+++ Curso de Python/Ejercicio 15/main.py
@@ -0,0 +1,10 @@
+import operaciones as op
+
+def main():
+    print(op.suma(2,4))
+    print(op.resta(10,4))
+    print(op.multiplicacion(10,2))
+    print(op.division(10,2))
+
+if(__name__ == '__main__'):
+    main()
