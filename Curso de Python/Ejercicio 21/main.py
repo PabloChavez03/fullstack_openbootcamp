062ce6dd538bde6bfd574ad34c2745d76b88bc7b
PabloChavez03 <pablooscarchavez@gmail.com>
2022-08-29 19:31:01 -0300

    ejercicio 21 terminado

--- Curso de Python/Ejercicio 21/main.py
+++ Curso de Python/Ejercicio 21/main.py
@@ -2,10 +2,24 @@ import tkinter
 
 window = tkinter.Tk()
 
-label_saludo = tkinter.Label(window, text="hola", bg="yellow", fg="blue")
-label_saludo.pack(ipadx=50,ipady=50,fill='x')
+valor = tkinter.StringVar()
 
-label_adios = tkinter.Label(window, text="adios", bg="green", fg="white")
-label_adios.pack(ipadx=50,ipady=100,fill='both')
+def reset(event):
+    valor.set('')
+
+r1 = tkinter.Radiobutton(window, text="Boton 1", value="Soy el boton 1", variable=valor)
+r2 = tkinter.Radiobutton(window, text="Boton 2", value="Soy el boton 2", variable=valor)
+r3 = tkinter.Radiobutton(window, text="Boton 3", value="Soy el boton 3", variable=valor)
+r1.pack(padx=10,pady=10)
+r2.pack(padx=10,pady=10)
+r3.pack(padx=10,pady=10)
+
+labelValor = tkinter.Label(window, textvariable=valor)
+labelValor.pack(padx=10,pady=10)
+
+botonSalir = tkinter.Button(window, text="Reset")
+botonSalir.pack()
+
+botonSalir.bind('<Button-1>', reset)
 
 window.mainloop()
